488682d3b79bdccbe1a9013d25f58f7d2aa56809
Roman <137167138+roma-dev-expert@users.noreply.github.com>
2025-05-30 21:06:32 +0300

    feat(infrastructure): add ApplicationDbContext with EF Core configuration

--- ChatApp.Infrastructure/Persistence/ApplicationDbContext.cs
+++ ChatApp.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using ChatApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection.Emit;
 
 namespace ChatApp.Infrastructure.Persistence
 {
-    internal class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        // Constructor receives DbContextOptions to configure the context
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSets for domain entities
+        public DbSet<User> Users { get; set; }
+        public DbSet<Chat> Chats { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<ChatUser> ChatUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure composite key for ChatUser entity
+            modelBuilder.Entity<ChatUser>()
+                .HasKey(cu => new { cu.ChatId, cu.UserId });
+
+            // Configure relationship: Chat has many ChatUsers
+            modelBuilder.Entity<ChatUser>()
+                .HasOne(cu => cu.Chat)
+                .WithMany(c => c.ChatUsers)
+                .HasForeignKey(cu => cu.ChatId);
+
+            // Configure relationship: User has many ChatUsers
+            modelBuilder.Entity<ChatUser>()
+                .HasOne(cu => cu.User)
+                .WithMany(u => u.ChatUsers)
+                .HasForeignKey(cu => cu.UserId);
+        }
     }
 }
